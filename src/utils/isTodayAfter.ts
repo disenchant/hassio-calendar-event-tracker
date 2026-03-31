e167e7544f4ed1d764e29e03a973cdb3474a60ca
Sven Vetsch <sven.vetsch@redguard.ch>
2026-03-31 11:59:44 +0200

    fix: resolve logic bug where events dropped prematurely due to seconds comparison

--- src/utils/isTodayAfter.ts
+++ src/utils/isTodayAfter.ts
@@ -1,21 +1,15 @@
 const isTodayAfter = (now: Date, dropAfter: string): boolean => {
   const [ hours, minutes, seconds ] = dropAfter.split(':');
 
-  if (now.getHours() < Number(hours)) {
-    return false;
-  }
-  if (now.getHours() > Number(hours)) {
-    return true;
+  if (now.getHours() !== Number(hours)) {
+    return now.getHours() > Number(hours);
   }
 
-  if (now.getMinutes() > Number(minutes)) {
-    return true;
-  }
-  if (now.getSeconds() >= Number(seconds)) {
-    return true;
+  if (now.getMinutes() !== Number(minutes)) {
+    return now.getMinutes() > Number(minutes);
   }
 
-  return false;
+  return now.getSeconds() >= Number(seconds);
 };
 
 export {
